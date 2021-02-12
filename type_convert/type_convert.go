e51bf39fa258670fc70f47f87408a47b836f7f94
Zhongshi Lai <laizhs@hotmail.com>
2021-02-12 18:30:55 +0800

    类型转换

--- type_convert/type_convert.go
+++ type_convert/type_convert.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"reflect"
+)
+
 func main() {
+	// 断言
+	// s, ok := x.(T)
+	// 把 x 转换为 T类型 s是接收的新类型的变量
+	_, err := os.Open("test_file")
+
+	if err != nil {
+		if e, ok := err.(*os.PathError); ok && e.Err != nil {
+			fmt.Println("=== type assert success ===")
+			fmt.Println(e)
+			fmt.Println(reflect.TypeOf(e))
+			fmt.Println("=== type assert success ===")
+		}
+	}
+
+	// switch 转换类型
+	switch i := err.(type) {
+	// type of i is type of x (interface{})
+	default:
+		fmt.Println(i)
+	}
 
 }
